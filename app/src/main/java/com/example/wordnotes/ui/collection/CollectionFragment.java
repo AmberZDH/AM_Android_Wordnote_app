10896dff9aeb4331fdda0f5ce5321d92cfda0a1a
Amber-D <amber.d@AmberMacBook-Pro.local>
2019-10-31 14:28:15 +0800

    Complete

--- app/src/main/java/com/example/wordnotes/ui/collection/CollectionFragment.java
+++ app/src/main/java/com/example/wordnotes/ui/collection/CollectionFragment.java
@@ -23,28 +23,26 @@ import java.util.ArrayList;
 public class CollectionFragment extends Fragment implements View.OnClickListener {
 
     private CollectionViewModel collectionViewModel;
-    private Optsql optsql;
+    private Optsql optsql;//连接数据库
     TextView input_textView;
-    TextView output_textView;
 
-    //    声明一个RecycleView变量
-//    RecyclerView recyclerView;
-    ArrayList<String[]> result=new ArrayList<>();
+    ArrayList<String[]> result = new ArrayList<>();
     String input_word;
+
+
+    RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_collection, container, false);
 
-        optsql=new Optsql(getActivity(), "Notebook.db", null, 1);
+        optsql = new Optsql(getActivity(), "Notebook.db", null, 1);
 
-//        recyclerView = (RecyclerView) root.findViewById(R.id.recycle_list);
-//
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//
-//        MyAdapter myAdapter = new MyAdapter();
-//
-//        recyclerView.setAdapter(myAdapter);
+
+        recyclerView = root.findViewById(R.id.recycle_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
 
 
         //注册 button 控件
@@ -65,84 +63,62 @@ public class CollectionFragment extends Fragment implements View.OnClickListener
         return root;
     }
 
+    public ArrayList<String[]> getFuzzy(String fuzzy){
+        return optsql.selectFuzzy(fuzzy);
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.collection_button:
-                System.out.println("\nokokkokokkkokokokokokokkokkokokookok\n");
+                System.out.println("\nokokkokokkkokokokokokokkokkokokokok\n");
                 this.input_word = input_textView.getText().toString();
-                System.out.println(input_word+"\n");
-                result=getFuzzy(this.input_word);
-                while(result.isEmpty()){
-                    String[] arr=result.get(0);
-                    System.out.println(arr[0]+"\n"+arr[1]+"\n"+arr[2]+"\n");
-                    result.remove(0);
+                System.out.println(input_word + "\n");
+                WordAdapter wordAdapter =new WordAdapter(getFuzzy(input_word));
+                recyclerView.setAdapter(wordAdapter);
+
+                break;
+
+        }
+
+    }
+
+
+    public class WordAdapter extends RecyclerView.Adapter<WordAdapter.ViewHolder> {
+        private ArrayList<String[]> mWordlist;
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView wordText;
+
+            public ViewHolder(@NonNull View view) {
+                super(view);
+                wordText = view.findViewById(R.id.text_list);
+
+
             }
+        }
 
+        public WordAdapter(ArrayList<String[]> wordlist) {
+            mWordlist = wordlist;
+        }
+
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list, parent, false);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.wordText.setText(mWordlist.get(position)[0]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mWordlist.size();
         }
     }
-
-    //    public setFuzzy(String fuzzy){
-//
-//    }
-    public ArrayList<String[]> getFuzzy(String fuzzy) {
-        try{
-        return optsql.selectFuzzy(fuzzy);}
-        catch (Exception e){
-            return null;
-        }
-    }
-
-
-//    //    自定义类继承RecycleView.Adapter类作为数据适配器
-//    class MyAdapter extends RecyclerView.Adapter {
-//
-//        @Override
-//        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//
-//
-//            MyHolder myHolder = new MyHolder(LayoutInflater.from(getContext()).inflate(R.layout.list, null));//引入自定义列表项的资源文件
-//
-//
-//            return myHolder;
-//        }
-//
-//
-//        @Override
-//        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-//            MyHolder mm = (MyHolder) holder;
-//
-////            将数据映射到控件中
-////            if (getFuzzy(input_word)[position][0] != null) {
-////                mm.te1.setText(getFuzzy(input_word)[position][0]);
-////            } else {
-//                mm.te1.setText("Hhhhhh Bugs");
-////            }
-//
-//
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-////            if ((getFuzzy(input_word)!=null)) {
-////                return getFuzzy(input_word).length;
-////            } else
-//                return 2;
-//        }
-//
-//        class MyHolder extends RecyclerView.ViewHolder {
-//
-//            TextView te1;
-//
-//            public MyHolder(View itemView) {
-//                super(itemView);
-//
-////                实例化子对象，把对象和列表项布局文件中的id绑定
-//                te1 = itemView.findViewById(R.id.text_list);
-//
-//            }
-//
-//        }
-//
-//    }
 }
